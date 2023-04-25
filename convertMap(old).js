87bcd18660749d0d86dd8f183a9364b341660342
ArsalanRehman <arsalanrahman008@gmail.com>
2023-04-25 16:56:07 +0300

    convertMap(old).js

---
+++ convertMap(old).js
@@ -0,0 +1,48 @@
+// exports.mapConvert = async (req, res) => {
+//   try {
+//     // getMap();
+//     const base64Data = req.body.imageBase64.replace(
+//       /^data:image\/[a-z]+;base64,/,
+//       ''
+//     );
+//     const mapWidth = req.body.width;
+//     const mapHeight = req.body.height;
+//     var imageType = req.body.imageBase64.match(
+//       /^data:image\/([a-z]+);base64,/
+//     )[1];
+//     if (!imageType) {
+//       imageType = 'png';
+//     }
+//     const filePath = `./images/convertedImage.${imageType}`;
+
+//     await writeFileAsync(filePath, base64Data, 'base64');
+
+//     //resize the image pixels
+
+//     await Jimp.read(filePath)
+//       .then((inputImage) => {
+//         // Resize the image to fit within a 400x400 square using Jimp
+//         inputImage.cover(mapWidth, mapHeight);
+
+//         // Write the resized image to disk
+//         const outputFilename = `images/convertedImage.${imageType}`;
+//         inputImage.write(outputFilename);
+//         console.log(`Resized image saved to ${outputFilename}`);
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//       });
+
+//     res.status(200).send({ message: 'Image saved successfully' });
+//     // console.log('timer 1');
+//     setTimeout(async () => {
+//       transferMapToROS();
+//     }, 1000);
+//     // console.log('timer 2');
+
+//     mapState = 'Map Converted';
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).send({ message: err });
+//   }
+// };
