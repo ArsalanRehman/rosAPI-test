f0ac3213b0cd86db3eba06aaf845719614c4401b
ArsalanRehman <arsalanrahman008@gmail.com>
2023-04-28 09:54:26 +0300

    polygonTest.js && rcl-getMap.js

--- polygonTest.js
+++ polygonTest.js
@@ -1,15 +1,48 @@
 const { createCanvas, loadImage } = require('canvas');
 const fs = require('fs');
+const sizeOf = require('image-size');
 
 const filename = 'images/map.jpg';
 const outputFilename = 'images/polygon.jpg';
 
 // Define the coordinates of the polygon vertices
+// const polygon = [
+//   [269.375, -521.8671875],
+//   [672.375, -517.8671875],
+//   [675.375, -190.8671875],
+//   [209.375, -177.8671875],
+// ];
+
+// Read the file into a buffer
+const buffer = fs.readFileSync(filename);
+
+// Get the dimensions of the image from the buffer
+const { width, height } = sizeOf(buffer);
+// console.log({ width, height });
+
+console.log(`${width} ${height}`);
+
+const centerX = width / 2; // x-coordinate of the center of the image
+const centerY = height / 2; // y-coordinate of the center of the image
+
+// const polygon = [
+//   [10, 10],
+//   [100, 10],
+//   [100, 100],
+//   [10, 100],
+// ];
+
+// const polygon = [
+//   [10 - centerX, 10 - centerY], // top-left vertex
+//   [100 - centerX, 10 - centerY], // top-right vertex
+//   [100 - centerX, 100 - centerY], // bottom-right vertex
+//   [10 - centerX, 100 - centerY], // bottom-left vertex
+// ];
 const polygon = [
-  [269.375, -521.8671875],
-  [672.375, -517.8671875],
-  [675.375, -190.8671875],
-  [209.375, -177.8671875],
+  [151, 151], // top-left vertex
+  [251, 151], // top-right vertex
+  [251, 251], // bottom-right vertex
+  [151, 251], // bottom-left vertex
 ];
 
 // Load the image
--- rclnodejs/getmap.js
+++ rclnodejs/getmap.js
@@ -1,17 +1,21 @@
 const rclnodejs = require('rclnodejs');
-const msg = rclnodejs.require('nav_msgs/msg/OccupancyGrid').msg;
+// const msg = rclnodejs.require('nav_msgs/msg/OccupancyGrid').msg;
 
 async function run() {
   try {
     await rclnodejs.init();
     const node = rclnodejs.createNode('subscriber_node');
     const sub = node.createSubscription(
-      'nav_msgs/msg/OccupancyGrid',
-      '/map',
+      'std_msgs/msg/String',
+      '/topic',
       (msg) => {
         console.log(msg);
       }
     );
+    setTimeout(() => {
+      console.log('Unsubscribing from the topic');
+      node.destroySubscription(sub);
+    }, 5000);
     rclnodejs.spin(node);
   } catch (error) {
     console.error(`Error: ${error}`);
