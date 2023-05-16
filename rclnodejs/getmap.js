11faac7ffb85afa84abc20a02a27ddadae8d4086
ArsalanRehman <arsalanrahman008@gmail.com>
2023-05-16 17:23:09 +0300

    rclnodejs/serviceTest.js added

--- rclnodejs/getmap.js
+++ rclnodejs/getmap.js
@@ -1,4 +1,5 @@
 const rclnodejs = require('rclnodejs');
+const Jimp = require('jimp');
 // const msg = rclnodejs.require('nav_msgs/msg/OccupancyGrid').msg;
 
 async function run() {
@@ -6,10 +7,30 @@ async function run() {
     await rclnodejs.init();
     const node = rclnodejs.createNode('subscriber_node');
     const sub = node.createSubscription(
-      'std_msgs/msg/String',
-      '/topic',
+      'nav_msgs/msg/OccupancyGrid',
+      '/map',
       (msg) => {
-        console.log(msg);
+        // console.log(msg);
+        map_width = msg.info.width;
+        map_height = msg.info.height;
+        console.log('width and height ', map_width, map_height);
+        const image = new Jimp(map_width, map_height);
+        for (let y = 0; y < map_height; y++) {
+          for (let x = 0; x < map_width; x++) {
+            const index = y * map_width + x;
+            const value = msg.data[index];
+            const invertedValue = value === 0 ? 255 : 0;
+            image.setPixelColor(
+              Jimp.rgbaToInt(invertedValue, invertedValue, invertedValue, 255),
+              x,
+              y
+            );
+          }
+        }
+        image.rotate(180);
+        image.mirror(true, false);
+        image.write(`images/rcl_map.jpg`);
+        node.destroySubscription(sub);
       }
     );
     setTimeout(() => {
---
+++ rclnodejs/serviceTest.js
@@ -0,0 +1,33 @@
+const rclnodejs = require('rclnodejs');
+
+async function main() {
+  // Initialize the ROS node
+  await rclnodejs.init();
+
+  // Create a Node
+  const node = rclnodejs.createNode('service_caller');
+
+  // Create a Client to call the service
+  const client = node.createClient(
+    'slam_toolbox/srv/Pause',
+    '/slam_toolbox/pause_new_measurements'
+  );
+
+  try {
+    // Create an empty request message (since the service doesn't require any inputs)
+    const request = new rclnodejs.example_msgs.srv.Trigger.Request();
+
+    // Call the service
+    await client.sendRequest(request);
+
+    console.log('Service triggered successfully!');
+  } catch (error) {
+    console.error('Service call failed:', error);
+  }
+
+  // Shutdown the ROS node
+  rclnodejs.shutdown();
+}
+
+// Run the main function
+main();
