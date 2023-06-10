0f094814a9aa9c6da7b1dc5a288b138bba83dbb1
ArsalanRehman <arsalanrahman008@gmail.com>
2023-06-10 10:53:20 +0300

    code refactor(sendGoalService)

--- rclnodejs/sendGoalService.js
+++ rclnodejs/sendGoalService.js
@@ -1,31 +1,4 @@
 const rclnodejs = require('rclnodejs');
-// rclnodejs
-//   .init()
-//   .then(() => {
-//     let node = rclnodejs.createNode('service_example_node');
-
-//     node.createClient(
-//       'amr_interfaces/srv/DriveGoal',
-//       '/drive_goal',
-//       (request, response) => {
-//         console.log(request);
-
-//         // Create a new goal_pose
-//         // let newGoalPose = {
-//         //   position: { x: 1, y: 2, z: 3 },
-//         //   orientation: { x: 0.707, y: 0, z: 0, w: 0.707 },
-//         // };
-
-//         // Send the new goal_pose as the response
-//         // response.send({ goal_pose: newGoalPose });
-//       }
-//     );
-
-//     rclnodejs.spin(node);
-//   })
-//   .catch((e) => {
-//     console.log(`Error: ${e}`);
-//   });
 
 rclnodejs
   .init()
@@ -42,19 +15,16 @@ rclnodejs
         orientation: { x: 0.0, y: 0.0, z: 0.0, w: 1.0 },
       },
     };
-
-    setInterval(() => {
-      client.waitForService(1000).then((result) => {
-        if (!result) {
-          console.log('Error: service not available');
-          return;
-        }
-        console.log(request);
-        client.sendRequest(request, (response) => {
-          console.log(`Result: ${typeof response}`, response);
-          rclnodejs.shutdown();
-        });
-      }, 100);
+    client.waitForService(1000).then((result) => {
+      if (!result) {
+        console.log('Error: service not available');
+        return;
+      }
+      console.log(request);
+      client.sendRequest(request, (response) => {
+        console.log(`Result: ${typeof response}`, response);
+        rclnodejs.shutdown();
+      });
     });
 
     rclnodejs.spin(node);
